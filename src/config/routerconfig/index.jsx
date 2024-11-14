469affea57f69605b1cb7702581cbeb678bfff31
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2024-11-15 00:36:45 +0300

    resetes

--- src/config/routerconfig/index.jsx
+++ src/config/routerconfig/index.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from '../../pages/home'
 import Aboutpage from '../../pages/about'
 import Brandpage from '../../pages/brand'
-import Contactpage from '../../pages/contact'
 import Uslugpage from '../../pages/uslug'
 import Mastercallpage from '../../pages/mastercall'
 export default function routerconfig() {
@@ -13,7 +12,6 @@ export default function routerconfig() {
      <Route path="/" element={<Homepage/>}></Route>
      <Route path="/about" element={<Aboutpage/>}></Route>
      <Route path="/brand" element={<Brandpage/>}></Route>
-     <Route path="/contact" element={<Contactpage/>}></Route>
      <Route path="/uslug" element={<Uslugpage/>}></Route>
      <Route path="/mastercall" element={<Mastercallpage/>}></Route>
     </Routes>
